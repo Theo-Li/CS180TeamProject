2ce908ce5ead76cdfddf1ec280f4cc864ec0efb7
lexborrero <135927700+lexborrero@users.noreply.github.com>
2025-04-05 20:09:57 -0400

    Created ItemManager

---
+++ src/ItemManager.java
@@ -0,0 +1,9 @@
+/**
+ * Message class represents a simple message exchanged between two users.
+ * Implements the IMessage interface.
+ *
+ * @author Tianzhi Li
+ * @version 2025-04-02
+ */
+public class ItemManager {
+}
--- src/PaymentStatus.java
+++ src/PaymentStatus.java
@@ -1,3 +1,10 @@
+/**
+ * Enum that represents the payment status
+ *
+ *
+ * @author Tianzhi Li
+ * @version 2025-04-02
+ */
 public enum PaymentStatus {
     PENDING,
     COMPLETED,
