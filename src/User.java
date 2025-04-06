1ececd00da6a54c270206feedae6806beb201f9e
lexborrero <135927700+lexborrero@users.noreply.github.com>
2025-04-06 14:58:48 -0400

    Changed User and and Added IUser

--- src/User.java
+++ src/User.java
@@ -1,5 +1,9 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import java.util.ArrayList;
+=======
+import java.util.List;
+>>>>>>> Stashed changes
 =======
 import java.util.List;
 >>>>>>> Stashed changes
@@ -12,6 +16,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @author L15 Team 1
  * @version March 1, 2025
  */
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
 public class User {
     private int userID;
@@ -67,10 +72,49 @@ public class User implements IUser {
         } else {
             // Atomically assigns a unique ID to this user and increments the counter for future instances.
 >>>>>>> Stashed changes
+=======
+public class User implements IUser {
+    // Unique identifier for the user.
+    private int userID;
+
+    // Thread-safe counter for generating unique user IDs for each new User instance.
+    private static AtomicInteger userCount = new AtomicInteger(0);
+
+    // The username chosen by the user.
+    private String username;
+
+    // The user's password (in production, passwords should be stored securely, e.g., hashed).
+    private String password;
+
+    // The account balance of the user.
+    private double balance;
+
+    // List of items associated with the user (e.g., items for sale or items owned).
+    private List<Item> items;
+
+    /**
+     * Constructs a new User instance with a username and password.
+     * It also assigns a unique userID using a thread-safe counter.
+     *
+     * @param username the username chosen by the user.
+     * @param password the user's password.
+     */
+    public User(String username, String password) {
+        // Set the username and password with the provided values.
+        this.username = username;
+        this.password = password;
+
+        // Although userCount is already initialized, this check is included (but is redundant).
+        if (userCount == null) {
+            userCount = new AtomicInteger(1);
+        } else {
+            // Atomically assigns a unique ID to this user and increments the counter for future instances.
+>>>>>>> Stashed changes
             userID = userCount.getAndIncrement();
         }
     }
 
+<<<<<<< Updated upstream
 <<<<<<< Updated upstream
     public synchronized int getUserID() {
         return userID;
@@ -104,6 +148,8 @@ public class User implements IUser {
 
 }
 =======
+=======
+>>>>>>> Stashed changes
     /**
      * Returns the unique identifier of the user.
      *
@@ -198,5 +244,9 @@ public class User implements IUser {
     public String toString() {
         return super.toString();
     }
+<<<<<<< Updated upstream
+}
+>>>>>>> Stashed changes
+=======
 }
 >>>>>>> Stashed changes
